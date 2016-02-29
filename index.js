690ad3ebb317805bce25bd3d0e0d5409985f4870
anativ <alon.nativ@gmail.com>
2016-02-29 15:20:13 +0200

    bug fix doens't extract the payload data on android

--- index.js
+++ index.js
@@ -156,12 +156,7 @@ Notifications._onNotification = function(data, isFromBackground = null) {
 		} else {
 			this.onNotification({
 				foreground: ! isFromBackground,
-				message: data.message,
-				data: (
-					typeof data.data !== 'undefined'
-					? data.data
-					: {} 
-				),
+				...data
 			});
 		}
 	}
