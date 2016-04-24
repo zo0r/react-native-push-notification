bdd63fae921bb31dc0cbd1cddcb530b932a4502f
zo0r <z@zo0r.me>
2016-04-23 19:08:20 -0500

    Make it compatible with Parse push by @frantic https://github.com/zo0r/react-native-push-notification/pull/22

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
@@ -9,11 +9,32 @@ import java.util.List;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RNPushNotificationListenerService extends GcmListenerService {
 
     @Override
     public void onMessageReceived(String from, Bundle bundle) {
+        JSONObject data = getPushData(bundle.getString("data"));
+        if (data != null) {
+            if (!bundle.containsKey("message")) {
+                bundle.putString("message", data.optString("alert", "Notification received"));
+            }
+            if (!bundle.containsKey("title")) {
+                bundle.putString("title", data.optString("title", null));
+            }
+        }
+
         sendNotification(bundle);
+    }
+
+    private JSONObject getPushData(String dataString) {
+        try {
+            return new JSONObject(dataString);
+        } catch (JSONException e) {
+            return null;
+        }
     }
 
     private void sendNotification(Bundle bundle) {
