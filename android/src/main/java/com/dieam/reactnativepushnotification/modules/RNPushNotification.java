cefbbbf729a24016cfea77dae8e9850803bf04bb
John Carlo Bambo <carlo@wizy.io>
2017-03-22 16:02:12 +0800

    * support mixed notification

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
@@ -58,9 +58,18 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
         return constants;
     }
 
+    private Bundle getNotificationBundle(Intent intent) {
+      if (intent.hasExtra("notification")) {
+        return intent.getBundleExtra("notification");
+      } else if (intent.hasExtra("google.message_id")) {
+        return intent.getExtras();
+      }
+      return null;
+    }
+
     public void onNewIntent(Intent intent) {
-        if (intent.hasExtra("notification")) {
-            Bundle bundle = intent.getBundleExtra("notification");
+      Bundle bundle = getNotificationBundle(intent);
+        if (bundle != null) {
             bundle.putBoolean("foreground", false);
             intent.putExtra("notification", bundle);
             mJsDelivery.notifyNotification(bundle);
@@ -141,7 +150,7 @@ public class RNPushNotification extends ReactContextBaseJavaModule implements Ac
         Activity activity = getCurrentActivity();
         if (activity != null) {
             Intent intent = activity.getIntent();
-            Bundle bundle = intent.getBundleExtra("notification");
+            Bundle bundle = getNotificationBundle(intent);
             if (bundle != null) {
                 bundle.putBoolean("foreground", false);
                 String bundleString = mJsDelivery.convertJSON(bundle);
