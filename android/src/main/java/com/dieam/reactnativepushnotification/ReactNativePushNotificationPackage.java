9e40fba2b0fd95097f5d64246169b637a4dbdaeb
Radek Czemerys <radko93@gmail.com>
2017-08-08 10:45:16 +0200

    Fix typo in comment

--- android/src/main/java/com/dieam/reactnativepushnotification/ReactNativePushNotificationPackage.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/ReactNativePushNotificationPackage.java
@@ -17,7 +17,7 @@ public class ReactNativePushNotificationPackage implements ReactPackage {
         return Collections.<NativeModule>singletonList(new RNPushNotification(reactContext));
     }
 
-    // Depreciated RN 0.47
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
