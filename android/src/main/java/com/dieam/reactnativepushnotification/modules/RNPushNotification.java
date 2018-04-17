2befc731011000fc5aff9c27a7d6963a49248e1a
Axel Vencatareddy <axel@koosmikcorp.com>
2018-04-17 14:24:03 +0200

    Add missing import (FirebaseMessaging)

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
@@ -26,6 +26,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class RNPushNotification extends ReactContextBaseJavaModule implements ActivityEventListener {
     public static final String LOG_TAG = "RNPushNotification";// all logging should use this tag
 
