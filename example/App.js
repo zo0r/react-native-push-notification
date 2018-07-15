28d368a1473df0cfa7ad9b0c6f7de0b7c9960619
Gp2mv3 <nicolascognaux@gmail.com>
2018-07-15 23:31:19 +0200

    First example app (Only on Android, tested only in emulator)

--- example/App.js
+++ example/App.js
@@ -1,23 +1,94 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import { TextInput, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import NotifService from './NotifService';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.notif = new NotifService(this.onRegister.bind(this), this.onNotif.bind(this));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+
+        <Text style={styles.title}>Example app react-native-push-notification</Text>
+        <View style={styles.spacer}></View>
+        <TextInput style={styles.textField} value={this.state.registerToken} placeholder="Register token" />
+        <View style={styles.spacer}></View>
+
+        <TouchableOpacity style={styles.button} onPress={() => {this.notif.localNotif()} }><Text>Local Notification (now)</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => {this.notif.scheduleNotif()} }><Text>Schedule Notification in 30s</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => {this.notif.cancelNotif()} }><Text>Cancel last notification (if any)</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => {this.notif.cancelAll()} }><Text>Cancel all notifications</Text></TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => {this.notif.checkPermission(this.handlePerm.bind(this))}}><Text>Check Permission</Text></TouchableOpacity>
       </View>
     );
   }
+
+  onRegister(token) {
+    console.log(token);
+    this.setState({ registerToken: token });
+  }
+
+  onNotif(notif) {
+    console.log(notif);
+    Alert.alert(notif.title, notif.message);
+  }
+
+  handlePerm(perms) {
+    Alert.alert("Permissions", JSON.stringify(perms));
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    margin: 5,
+    padding: 5,
+    width: "70%",
+    backgroundColor: "#DDDDDD",
+    borderRadius: 5,
+  },
+  textField: {
+    borderWidth: 1,
+    borderColor: "#AAAAAA",
+    margin: 5,
+    padding: 5,
+    width: "70%"
+  },
+  spacer: {
+    height: 10,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  }
 });
