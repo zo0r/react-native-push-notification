98e4e4dc0c0245147fa9e6c5a128cd2dc1030fd5
Ray Deck <ray@raydeck.com>
2019-01-13 11:22:53 -0500

    Updating core

--- prelink.js
+++ prelink.js
@@ -28,7 +28,7 @@ if (!fs.existsSync(projectGradlePath)) {
   process.exit(1);
 }
 const ptext = fs.readFileSync(projectGradlePath, { encoding: "UTF8" });
-const newClassPathText = "classpath 'com.google.gms:google-services:4.0.2'";
+const newClassPathText = "classpath 'com.google.gms:google-services:4.2.0'";
 if (ptext.indexOf(newClassPathText) === -1) {
   var plines = ptext.split("\n");
 
