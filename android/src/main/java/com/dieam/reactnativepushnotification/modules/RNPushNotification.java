f8a00487755a39d4b03ad7b6d435ab2eac82eb37
Boris Tacyniak <boris.tacyniak@free.fr>
2020-05-25 09:52:24 +0200

    Remove extra interface.

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotification.java
@@ -38,7 +38,7 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class RNPushNotification extends ReactContextBaseJavaModule implements ActivityEventListener , Application.ActivityLifecycleCallbacks{
+public class RNPushNotification extends ReactContextBaseJavaModule implements ActivityEventListener {
     public static final String LOG_TAG = "RNPushNotification";// all logging should use this tag
 
     private RNPushNotificationHelper mRNPushNotificationHelper;
