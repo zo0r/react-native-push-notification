38ce97506b783664ecfcc9ce748f03720642bc56
Anatoliy Magda <40770513+aamagda@users.noreply.github.com>
2023-01-20 20:09:59 +0300

    Add ReceivedMessageCustomHandler interface

---
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNReceivedMessageCustomHandler.java
@@ -0,0 +1,7 @@
+package com.dieam.reactnativepushnotification.modules;
+
+import android.os.Bundle;
+
+public interface RNReceivedMessageCustomHandler {
+    void onMessageReceived(Bundle bundle);
+}
